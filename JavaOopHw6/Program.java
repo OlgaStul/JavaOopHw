1813c0d070aedf1cc67062cb088ac2b7fbb777fa
Olga <chi89@yandex.ru>
2023-06-23 12:44:44 +0300

    solid, принцип srp на примере заказа продукта

--- JavaOopHw6/Program.java
+++ JavaOopHw6/Program.java
@@ -2,6 +2,8 @@ package JavaOopHw6;
 
 public class Program {
     public static void main(String[] args) {
+
+        System.out.println("Новый заказ.");
         Order order = new Order();
         DataInput dataInput = new DataInput();
         SavingData savingData = new SavingData();
